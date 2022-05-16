9e9909924494107a2ef6c069b0676dfebafc8f66
Erich26 <erich.fredersdorff@codeimmersives.com>
2022-05-16 14:35:51 -0400

    extra routes

--- app.js
+++ app.js
@@ -3,7 +3,27 @@ const app = express()
 const port = 4000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Erich Fredersdorff')
+})
+
+app.get('/search', (req, res) => {
+    res.status(200).send('This is a page.')
+})
+
+app.get('/unauthorized', (req, res) => {
+    res.status(401).send('unauthorized route')
+})
+
+app.get('/forbidden', (req, res) => {
+    res.status(403).send('forbidden route')
+})
+
+app.get('/notacceptable', (req, res) => {
+    res.status(406).send('not acceptable route')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('This route has not been found.')
 })
 
 app.listen(port, () => {
